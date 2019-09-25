dadb7cd1b9ab4e59479f80def64b5193ff497a06
Karan <karanparekh501@gmail.com>
2019-09-25 23:49:03 +0530

    basic app functionality compplete

--- view_image.py
+++ view_image.py
@@ -1,11 +1,8 @@
 import os
 import pydicom
 import matplotlib.pyplot as plt
-from tkinter import Tk, Button
-from tkinter.filedialog import askopenfile
-
-data_dir = 'dicom_files/'
-patients = os.listdir(data_dir)
+from tkinter import Tk, Button, Listbox, END, Scrollbar, Frame, RIGHT
+from tkinter.filedialog import askopenfile, askdirectory
 
 
 def view(path):
@@ -15,14 +12,41 @@ def view(path):
     plt.show()
 
 
-def open_dicom():
-    patient = askopenfile(mode='r', filetypes=[('DICOM file', '*.dcm')]).name
-    view(patient)
+# def open_dicom():
+#     patient = askopenfile(mode='r', filetypes=[('DICOM file', '*.dcm')]).name
+#     view(patient)
 
 
 root = Tk(className="DICOM Viewer")
 
-file_path = Button(root, text="Choose File (.dcm)", padx=5, pady=5, command=open_dicom)
+frm = Frame(root)
+frm.pack()
+
+geo = root.geometry
+geo("300x300")
+
+
+def open_folder():
+
+    def selected(event):
+        path = dcm_list.get(dcm_list.curselection())
+        view(folder + "/" + path)
+
+    folder = askdirectory()
+    print(folder)
+    flist = os.listdir(folder)
+
+    dcm_list = Listbox(frm, width=20)
+    dcm_list.bind("<<ListboxSelect>>", selected)
+    dcm_list.pack()
+    for f in flist:
+        dcm_list.insert(END, f)
+
+
+file_path = Button(root, text="Choose Folder (.dcm)", padx=5, pady=5, command=open_folder)
 file_path.pack()
+
+
+# view_button = Button(root, text="View", command=view, args=selected_dcm)
 
 root.mainloop()
