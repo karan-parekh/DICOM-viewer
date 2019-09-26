8961e2bf0c55c061ce11274b9fcc8f7b0b3c388d
Karan <karanparekh501@gmail.com>
2019-09-26 15:52:33 +0530

    added info label

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pydicom
 import matplotlib.pyplot as plt
-from tkinter import Tk, Button, Listbox, Scrollbar, Frame, Label, END, NS, NW
+from tkinter import Tk, Button, Listbox, Scrollbar, Frame, Label, StringVar, END, NS, NW
 from tkinter.filedialog import askdirectory
 
 
@@ -14,11 +14,14 @@ class Viewer(Frame):
         # frame for Listbox and Scrollbar
         self.frm = Frame(self)
         self.frm.grid(row=2, sticky=NW)
-        # Button and Label
+        # Button and Labels
         self.file_path = Button(self, text="Choose Folder", command=self.set_dcm_files)
         self.file_path.grid(row=0)
-        self.lab = Label(self, text="Double click the folder \n and then click OK")
-        self.lab.grid(row=1)
+        self.msg_label = Label(self, text="Double click the folder \n and then click OK")
+        self.msg_label.grid(row=1)
+        self.info = StringVar(self)
+        self.info_label = Label(self.frm, textvariable=self.info)
+        self.info_label.grid(row=2)
         # Declaring Scrollbar and Listbox
         self.sbar = Scrollbar(self.frm, orient='vertical')
         self.dcm_list = Listbox(self.frm, width=20, yscrollcommand=self.sbar.set)
@@ -42,13 +45,22 @@ class Viewer(Frame):
 
     # Views the selected item from the Listbox
     def view_selected_item(self, event):
-        path = self.dcm_list.get(self.dcm_list.curselection())
-        dataset = pydicom.read_file(self.folder + "/" + path)
-        plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
-        plt.show()
+        file = self.dcm_list.get(self.dcm_list.curselection())
+        if file.split(".")[1] == 'dcm':
+            self.show_info(file)
+            dataset = pydicom.read_file(self.folder + "/" + file)
+            plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
+            plt.show()
+        else:
+            self.show_info(f"{file} is not a .dcm file")
+
+    # Display info below the Listbox
+    def show_info(self, text=""):
+        self.info.set(text)
+        self.info_label.grid(row=2)
 
 
 if __name__ == '__main__':
-    root = Tk(className="DICOM Viewer")
+    root = Tk(className=" DICOM Viewer")
     Viewer(root).pack(fill='both', expand=True)
     root.mainloop()
