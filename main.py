3bdac0357bc5c2ac04277987c0fc823dc981e513
Karan <karanparekh501@gmail.com>
2019-09-26 15:07:31 +0530

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pydicom
 import matplotlib.pyplot as plt
-from tkinter import Tk, Button, Listbox, Scrollbar, Frame, Label, RIGHT, END, NS, NW
+from tkinter import Tk, Button, Listbox, Scrollbar, Frame, Label, END, NS, NW
 from tkinter.filedialog import askdirectory
 
 
@@ -11,32 +11,36 @@ class Viewer(Frame):
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
+        # frame for Listbox and Scrollbar
         self.frm = Frame(self)
         self.frm.grid(row=2, sticky=NW)
-
-        self.file_path = Button(self, text="Choose Folder", command=self.get_all_files)
+        # Button and Label
+        self.file_path = Button(self, text="Choose Folder", command=self.set_dcm_files)
         self.file_path.grid(row=0)
         self.lab = Label(self, text="Double click the folder \n and then click OK")
         self.lab.grid(row=1)
-
-        self.sb = Scrollbar(self.frm, orient='vertical')
-        self.dcm_list = Listbox(self.frm, width=20, yscrollcommand=self.sb.set)
+        # Declaring Scrollbar and Listbox
+        self.sbar = Scrollbar(self.frm, orient='vertical')
+        self.dcm_list = Listbox(self.frm, width=20, yscrollcommand=self.sbar.set)
         self.dcm_list.bind("<<ListboxSelect>>", self.view_selected_item)
-        self.sb.config(command=self.dcm_list.yview)
-
+        self.sbar.config(command=self.dcm_list.yview)
+        # Rendering Listbox and Scrollbar
         self.dcm_list.grid(row=0, column=0)
-        self.sb.grid(row=0, column=1, sticky=NS)
+        self.sbar.grid(row=0, column=1, sticky=NS)
 
-    def get_all_files(self):
+    # Sets dcm files and populates Listbox
+    def set_dcm_files(self):
         self.folder = askdirectory()
         self.files = os.listdir(self.folder)
         self.populate_list()
 
+    # Populates the Listbox
     def populate_list(self):
         self.dcm_list.delete(0, 'end')
         for f in self.files:
             self.dcm_list.insert(END, f)
 
+    # Views the selected item from the Listbox
     def view_selected_item(self, event):
         path = self.dcm_list.get(self.dcm_list.curselection())
         dataset = pydicom.read_file(self.folder + "/" + path)
